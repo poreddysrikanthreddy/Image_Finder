71195024346cd9683499a4f951155f188f686203
poreddysrikanthreddy <poreddisrikanth@gmail.com>
2024-05-22 16:56:56 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import postsReducer from '../features/posts/postsSlice';
-// import usersReducer from '../features/users/usersSlice';
+
 import usersReducer from "../user/userSlice";
 
 export const store = configureStore({
